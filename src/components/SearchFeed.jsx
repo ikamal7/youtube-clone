460c0b84dcd358bbc3bc0d406f3a30444a2f7fd9
Kamal Hosen <kamalhosen8920@gmail.com>
2022-09-01 18:17:52 +0600

    single video page

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos } from "./";
+import { useParams } from "react-router-dom";
+
 
 const SearchFeed = () => {
+
+  const [ videos, setVideos ] = useState();
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    
+    setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${ searchTerm }`)
+      .then((data) => setVideos(data.items));
+    }, [ searchTerm ]
+  );
+
   return (
-    <div>SearchFeed</div>
-  )
+    <Stack sx={ {
+      flexDirection: { sx: "column", md: "row" },
+    }}>
+      <Box p={ 2 } sx={ {
+        overflowY: 'auto',
+        height: '90vh',
+        flex: '2'
+      }}>
+        <Typography variant="h4" fontWeight="bold" mb="2" sx={ {
+          color: 'white'
+        }}>
+          Search Results for: <span style={{color: '#FC1503'}}> {searchTerm} </span> videos
+        </Typography>
+
+        <Videos videos={ videos } />
+
+      </Box>
+    </Stack>
+  );
 }
 
-export default SearchFeed
+export default SearchFeed;
--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import ReactPlayer from 'react-player';
+import { Link, useParams } from 'react-router-dom';
+import { Box, Stack, Typography } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+import Videos from './Videos';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+import Loader from './Loader';
+
+
+
 
 const VideoDetail = () => {
+  const [ videoDetail, setVideoDetail ] = useState(null);
+  const [ videos, setVideos ] = useState(null);
+  const { id } = useParams();
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${ id }`)
+      .then((data) => setVideoDetail(data.items[ 0 ]));
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${ id }&type=video`)
+      .then((data) => setVideos(data.items));
+  },
+    [ id ]);
+
+  if (!videoDetail?.snippet) return <Loader />;
+
+  const { snippet: {title, channelId, channelTitle}, statistics:{viewCount, likeCount}} = videoDetail;
+
   return (
-    <div>VideoDetail</div>
+    <Box minHeight='95vh'>
+      <Stack direction={{xs:'column', md: 'row'}}>
+        <Box flex="1">
+          <Box sx={ { width: '100%', position: 'sticky', top: '86px' } }>
+            <ReactPlayer url={ `//www.youtube.com/watch?v=${ id }` }
+              className="react-player" controls config={{ youtube: { playerVars: { origin: 'https://www.youtube.com' } } }}
+            />
+            <Typography color='#fff' variant='h5' fontWeight="bold" p={2}>
+              {title}
+            </Typography>
+            <Stack direction="row" justifyContent="space-between" sx={{color: '#fff'}} py={1} px={2}>
+              <Link to={ `/channel/${ channelId }` }>
+                <Typography color='#FFF' variant={ { sm: 'subtitle1', md: 'h6' } }>
+                  { channelTitle }
+                  <CheckCircle sx={{fontSize:14, color: 'gray', ml:'5px' }} />
+                </Typography>
+                </Link>
+            </Stack>
+            <Stack direction='row' gap="20px" alignItems="center">
+              <Typography variant='body1' sx={{opacity: '0.7'}}>
+                {parseInt(viewCount).toLocaleString()} views
+              </Typography>
+              <Typography variant='body1' sx={{opacity: '0.7'}}>
+                {parseInt(likeCount).toLocaleString()} likes
+                </Typography>
+            </Stack>
+          </Box>
+        </Box>
+        <Box px={2} py={{ md: 1, xs: 5 }} justifyContent="center" alignItems='center'>
+            <Videos videos={videos} direction="column" />
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
-export default VideoDetail
+export default VideoDetail;

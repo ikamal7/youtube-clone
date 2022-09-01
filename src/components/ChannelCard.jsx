404ab81476cfe2362907de7800952c3f6aa03e93
Kamal Hosen <kamalhosen8920@gmail.com>
2022-09-01 14:57:54 +0600

    Channel details and videos

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import { demoProfilePicture  } from '../utils/constants'
 
 
-const ChannelCard = ({ channelDetail }) => (
+const ChannelCard = ({ channelDetail, marginTop }) => (
   <Box
     sx={ {
       boxShadow: 'none',
@@ -15,7 +15,8 @@ const ChannelCard = ({ channelDetail }) => (
       alignItems: 'center',
       width: { xs: '356px', md: '320px' },
       height: '326px',
-      margin: 'auto'
+      margin: 'auto',
+      marginTop
       
     } }
   >
--- src/components/ChannelDetails.jsx
+++ src/components/ChannelDetails.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { VideoCard, ChannelCard, Videos } from './';
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetails = () => {
+
+  const [channelDetail, setChannelDetail] = useState(null)
+  const [videos, setvideos] = useState([])
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${ id }`)
+      .then((data) => setChannelDetail(data?.items[ 0 ]));
+    
+    fetchFromAPI(`search?channelId=${ id }&part=snippet&order=date`)
+      .then((data) => setvideos(data?.items));
+  }, [ id ]);
   return (
-    <div>ChannelDetails</div>
+    <Box minHeight='95vh'>
+      <Box>
+        <div style={ {
+          background : 'linear-gradient(90deg, rgba(42,213,212,1) 0%, rgba(19,22,144,1) 100%)',
+          zIndex: 10,
+          height:'300px'
+        }}
+        />
+        <ChannelCard channelDetail={ channelDetail } marginTop="-110px" />
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{mr: {sm: '100px'}}} />
+          <Videos videos={videos} />
+      </Box>
+    </Box>
   )
 }
 
-export default ChannelDetails
+export default ChannelDetails;
